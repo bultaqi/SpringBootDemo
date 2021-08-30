ce1bd16a55bb9fe5ff073095e576c538570da8fa
bultaqi <bultaqi@gmail.com>
2021-08-30 09:28:13 -0400

    new db connection and added transient annotation to allow the autmatic calculation of age.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -6,8 +6,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.lang.reflect.Constructor;
 import java.time.LocalDate;
+import java.time.Period;
 
 
 @Entity
@@ -27,24 +29,25 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+
+    // Transient tells the DB that there is no need to be stored and is calculated first
+    @Transient
     private Integer age;
 
     public Student() {
     }
 
-    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+    public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -80,7 +83,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
